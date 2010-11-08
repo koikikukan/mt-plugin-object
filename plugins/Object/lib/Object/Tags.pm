ecb89296838d8303be81ddf74ef3fca0cc662f09
koikikukan <youjiroh@wd5.so-net.ne.jp>
2010-11-09 01:48:55 +0900

    first commit

---
+++ plugins/Object/lib/Object/Tags.pm
@@ -0,0 +1,62 @@
+package Object::Tags;
+
+use strict;
+
+sub _hdlr_objects {
+    my ($ctx, $args, $cond) = @_;
+ 
+    my $name = $args->{name};
+    return '' if !$name;
+    my $class = MT->model($name);
+    return '' if !$class;
+
+    my $res = '';
+    my $builder = $ctx->stash('builder');
+    my $tokens = $ctx->stash('tokens');
+
+    my ($term, $arg);
+
+    my $blog = $ctx->stash('blog');
+    $term->{blog_id} = $blog->id;
+
+    if ($class =~ /Blog|Author|Session|Tag/) {
+        undef $term;
+    }
+    if ($class eq 'MT::Asset') {
+        $term->{class} = '*';
+    }
+
+    my @objects = $class->load($term, $arg);
+    return '' if !@objects;
+
+    my $count = 0;
+    my $max = scalar @objects;
+    my $vars = $ctx->{__stash}{vars} ||= {};
+    for my $object (@objects) {
+        $count++;
+        local $ctx->{__stash}{$name} = $object;
+        local $vars->{__first__} = $count == 1;
+        local $vars->{__last__} = ($count == ($max));
+        local $vars->{__odd__} = ($count % 2) == 1;
+        local $vars->{__even__} = ($count % 2) == 0;
+        local $vars->{__counter__} = $count;
+        my $out = $builder->build($ctx, $tokens, { %$cond });
+        return $ctx->error( $builder->errstr ) unless defined $out;
+        $res .= $out;
+    }
+    $res;
+
+}
+
+sub _hdlr_object_data {
+    my ($ctx, $args) = @_;
+    my $name = $args->{name};
+    my $prop = $args->{property};
+    return '' unless $prop;
+
+    my $object = $ctx->stash($name)
+        or return $ctx->error();
+    return $object->$prop;
+}
+
+1;
